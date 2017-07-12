5b9fc3fc873be519624288dad37947d22a11a5a5
Yun-hyunyoung <yhy3587@gmail.com>
2017-07-12 20:37:49 +0900

    artwork data spreading on index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '36.5 Arts', name: getName(req.session.email)});
+    User.find({artworks: {$exists: true}}, {artworks: true}, function (err, users) {
+        var artworks = [];
+        for (var i = 0; i < users.length; i++){
+            var artwork = users[i].artworks;
+            for (var j = 0; j < artwork.length; j++){
+                artworks[artworks.length] = artwork[j];
+            }
+        }
+        console.log(artworks);
+        res.render('index', { title: '36.5 Arts', name: getName(req.session.email), artworks : artworks});
+    });
 });
 
 function getName(email) {

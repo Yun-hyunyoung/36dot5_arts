741915ea77297791602194e9ed36eccd9d72da61
DESKTOP-FGIGLTB\jinpy <jinpyo0311@gmail.com>
2017-08-09 19:45:28 +0900

    all_artworks

--- routes/all_artworks.js
+++ routes/all_artworks.js
@@ -10,9 +10,24 @@ var Artwork = require('../models/artwork');
 
 
 
-router.get('/all_artworks', function(req, res) {
+router.get('/', function(req, res) {
+  User.find({artworks: {$exists: true}}, {artworks: true}, function (err, users) {
+      var artworks = [];
+      for (var i = 0; i < users.length; i++){
+          var artwork = users[i].artworks;
+          for (var j = 0; j < artwork.length; j++){
+              artworks[artworks.length] = artwork[j];
+          }
+      }
 
-        res.render('all_artworks');
+      artworks.sort(function (a, b) {
+          return b.regist_date - a.regist_date;
+      });
+
+
+      console.log(req.session.email);
+      res.render('all_artworks', { title: '36.5 Arts', nickname: users.nickname, session_email: req.session.email, artworks : artworks});
+  });
     });
 
 module.exports = router;
